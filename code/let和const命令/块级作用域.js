814abf9b398e3e3816d3156cf1f0ee7fb6bc9261
qq2575896094 <2575896094@qq.com>
2017-10-27 16:02:54 +0800

    提交者:雨田 时间:2017年10月27日16:02:48 版本:v1.0.2 描述:新增块级作用域语法

--- code/let和const命令/块级作用域.js
+++ code/let和const命令/块级作用域.js
@@ -1,3 +1,47 @@
 /**
  * Created by qq2575896094 on 2017/10/27.
  */
+
+{
+    let a = 1;
+    {
+        let b = 2;
+        console.log("a:" + a);
+        console.log("b:" + b);
+    }
+    console.log("a:" + a);
+}
+// console.log("a:" + a);      //ReferenceError: a is not defined
+
+console.log("==========================================================");
+
+(function (num1, num2) {
+    console.log(num1 + num2);
+    console.log("立即执行函数");
+})(1, 2);
+
+{
+    let num1 = 1;
+    let num2 = 2;
+    console.log(num1 + num2);
+    console.log("代码块");
+}
+
+console.log("==========================================================");
+
+{
+    let a = '12';
+
+    //函数声明语句
+    function f() {
+        return a;
+    }
+}
+
+{
+    let a = 12;
+    //函数表达式
+    let f = function () {
+        return a;
+    }
+}

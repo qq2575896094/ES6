cb1ec74fc302d574387afa773917ec0e57c3ee3e
qq2575896094 <2575896094@qq.com>
2017-10-27 11:22:19 +0800

    提交者:雨田 时间:2017年10月27日11:22:11 版本:v1.0.1 描述:新增let命令语法

--- code/let和const命令/let命令.js
+++ code/let和const命令/let命令.js
@@ -1,8 +1,11 @@
 /**
  * Created by qq2575896094 on 2017/10/27.
  *
- *  let声明的变量只在它所在的代码块有效。
+ *  1. let声明的变量只在它所在的代码块有效。
+ *  2. 不存在变量提升
  */
+
+console.log("==========================================================");
 
 let a = 1;
 var b = 2;
@@ -16,3 +19,53 @@ var b = 2;
 }
 console.log(a);                         // 1
 console.log(b);                         // 3
+
+
+console.log("==========================================================");
+
+let arrA = [];
+let arrB = [];
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    arrA[i] = function () {
+        console.log(i);
+    };
+}
+
+for (var j = 0; j < 10; j++) {
+    arrB[j] = function () {
+        console.log(j);
+    };
+}
+// console.log(i);     //ReferenceError: i is not defined
+arrA[5]();          //5
+arrB[5]();          //10
+console.log(j);     //10
+
+console.log("==========================================================");
+
+console.log("aa: " + aa);   //aa: undefined
+// console.log("bb: " + bb);   //ReferenceError: bb is not defined
+var aa = 2;
+let bb = 1;
+console.log("aa: " + aa);   //aa: 2
+console.log("bb: " + bb);   //bb: 1
+
+console.log("==========================================================");
+
+var tmp = '1';
+if (true) {
+    // tmp = "tmp";        //ReferenceError: tmp is not defined
+    let tmp;
+}
+
+console.log("==========================================================");
+
+{
+    let q = 1;
+    // var q = 2;      //SyntaxError: Identifier 'q' has already been declared
+}
+{
+    let w = 3;
+    // let w = 4;  //SyntaxError: Identifier 'w' has already been declared
+}
